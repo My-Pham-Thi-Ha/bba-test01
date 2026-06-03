8cb15061ac89ffa7f6603a5030cc0168d5616f86
test 01 - my <phamhamy.hvtc@gmail.com>
2026-06-03 15:20:47 +0700

    feat: add solution for challenge 03

---
+++ tests/lesson-04/cloud-kingdom.js
@@ -0,0 +1,10 @@
+let playerName = "Mario";
+let currentLives = 3;
+const coinLevel1 = 25;
+const coinLevel2 = 30;
+const coinLevel3 = 45;
+
+const sumOfCoin = coinLevel1 + coinLevel2 + coinLevel3;
+const coinAverage = sumOfCoin / 3;
+const sodu = sumOfCoin % 3;
+console.log(`So du khi chia tong so coin cho 3: ${sodu}`);
